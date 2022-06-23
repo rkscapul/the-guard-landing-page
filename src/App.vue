6a55c6d411806baf561c36a9a9c8564fd345186e
rcoder3270 <rcoder3270@gmail.com>
2022-06-23 13:32:21 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1 class="blue">The Guard Bot</h1> 
+    <h3 class="black">oh hello</h3>
+  </header>
+</template>
+
+<style>
+  @import './assets/base.css';
+
+  #app {
+    margin: auto;
+    padding: 5rem;
+
+    font-weight: normal;
+  }
+
+  header {
+    line-height: 1.5;
+  }
+
+  .logo {
+    display: block;
+    margin: 0 auto 2rem;
+  }
+
+  .blue {
+    text-decoration: none;
+    color: rgb(29, 136, 150);
+    transition: 0.4s;
+  }
+
+  .black {
+    text-decoration: none;
+    color: rgb(46, 46, 46);
+    transition: 0.4s;
+  }
+</style>
